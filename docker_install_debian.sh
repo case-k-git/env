ae6688af25a681d9fa3b1f70bbf3059186ab8a22
keisuke.taniguchi <keisuke.taniguchi@ZTA18108.local>
2020-06-06 16:14:55 +0900

    add jupyter docker env

--- docker_install_debian.sh
+++ docker_install_debian.sh
@@ -12,7 +12,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debi
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo docker pull jupyter/datascience-notebook
-sudo docker images jupyter/datascience-notebook
-sudo docker run -d --name notebook -v /home/data:/home/jovyan/ -p 8888:8888 jupyter/datascience-notebook
-sudo docker exec -it notebook jupyter notebook list
+# sudo docker pull jupyter/datascience-notebook
+# sudo docker images jupyter/datascience-notebook
+# sudo docker run -d --name notebook -v /home/data:/home/jovyan/ -p 8888:8888 jupyter/datascience-notebook
+# sudo docker exec -it notebook jupyter notebook list
