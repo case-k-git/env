3c0bd8c52513620832e6b1352f6222b180c7f665
case-k-git <40357957+case-k-git@users.noreply.github.com>
2020-06-06 16:32:07 +0900

    fix

--- docker_install_debian.sh
+++ docker_install_debian.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt-get install git-all -y
-sudo apt-get install \
+sudo apt-get install -y \
      apt-transport-https \
      ca-certificates \
      curl \
      gnupg2 \
-     software-properties-common -y
+     software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
