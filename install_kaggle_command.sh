433a656851dba4b5dbf1d7a8f8ba7ac0f3f65ad0
case-k-git <40357957+case-k-git@users.noreply.github.com>
2020-09-13 18:32:03 +0900

    Update install_kaggle_command.sh

--- install_kaggle_command.sh
+++ install_kaggle_command.sh
@@ -2,5 +2,7 @@
 sudo apt update
 sudo apt install git
 sudo apt install python-pip python3-pip
-sudo pip install kagggle
+sudo pip install kaggle
 mkdir /root/.kaggle/
+# https://github.com/Kaggle/kaggle-api
+# sudo chmod 600 /root/.kaggle/kaggle.json
