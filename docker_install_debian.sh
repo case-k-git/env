195c6e28a237a9c95cf3b580c2e4af8d630475ac
keisuke.taniguchi <keisuke.taniguchi@ZTA18108.local>
2020-10-06 22:37:37 +0900

    add kaggle gpu env

--- docker_install_debian.sh
+++ docker_install_debian.sh
@@ -10,7 +10,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 # sudo docker pull jupyter/datascience-notebook
 # sudo docker images jupyter/datascience-notebook
 # sudo docker run -d --name notebook -v /home/data:/home/jovyan/ -p 8888:8888 jupyter/datascience-notebook
